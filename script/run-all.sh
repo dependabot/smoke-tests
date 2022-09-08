f559ae523873040f96dcdcbb24a9523917ae0ad0
jakecoffman <jake@jakecoffman.com>
2022-09-08 11:28:05 -0500

    add missing scripts

---
+++ script/run-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is useful for regenerating all of the smoke tests running locally.
+
+declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "gradle" "hex" "maven" "npm" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform")
+for eco in "${arr[@]}"
+do
+  dependabot test -f "tests/smoke-$eco.yaml" -o "testdata/smoke-$eco.yaml"
+done
