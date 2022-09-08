f4fa0908427fcd318209012efd03ad2a48e531fd
jakecoffman <jake@jakecoffman.com>
2022-09-08 11:38:05 -0500

    actions doesn't honor the commit

--- script/run-all.sh
+++ script/run-all.sh
@@ -5,5 +5,5 @@
 declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "gradle" "hex" "maven" "npm" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform")
 for eco in "${arr[@]}"
 do
-  dependabot test -f "tests/smoke-$eco.yaml" -o "testdata/smoke-$eco.yaml"
+  dependabot test -f "tests/smoke-$eco.yaml" -o "tests/smoke-$eco.yaml"
 done
