e5e27d5c15d017aac6ef5d6091e66ed9e8176cdf
jakecoffman <jake@jakecoffman.com>
2022-09-29 09:39:57 -0500

    configure to use token

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,5 @@
 gh release download --repo dependabot/cli -p "*linux-amd64.tar.gz"
 tar xzvf *.tar.gz >/dev/null 2>&1
 sudo mv dependabot /usr/local/bin
+
+echo "export LOCAL_GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN" >> ~/.bashrc
