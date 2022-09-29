d0f511dfa9f210b6a240f738471021235a0aa321
jakecoffman <jake@jakecoffman.com>
2022-09-29 07:10:03 -0500

    fix CLI download location

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -6,7 +6,7 @@ set -e
 # Anything that should not be shared between debugger sessions should happen here.
 
 # Download CLI
-gh release download --repo dependabot/cli-releases -p "*linux-amd64.tar.gz"
+gh release download --repo dependabot/cli -p "*linux-amd64.tar.gz"
 tar xzvf ./*.tar.gz >/dev/null 2>&1
 rm ./*.tar.gz
 mv dependabot /usr/local/bin
