37c78f7c78f1988f357cb4a1ed518266e9f57b98
Jake Coffman <jake@jakecoffman.com>
2023-02-10 09:31:44 -0600

    no need to pull the big updater image

--- .devcontainer/on-create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This pull takes a while, adding it to the prebuild
-docker pull ghcr.io/dependabot/dependabot-updater:latest
-docker pull ghcr.io/github/dependabot-update-job-proxy/dependabot-update-job-proxy:latest
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 gh release download --repo dependabot/cli -p "*linux-amd64.tar.gz"
-tar xzf *.tar.gz
+tar xzf ./*.tar.gz
 mkdir -p ~/.local/bin
 mv dependabot ~/.local/bin
-rm *.tar.gz
+rm ./*.tar.gz
 
 echo "export LOCAL_GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN" >> ~/.bashrc
