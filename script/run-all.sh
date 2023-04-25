29bbe6dd93612b97d6cc994a37982b350d3968d1
Bryan Dragon <25506+bdragon@users.noreply.github.com>
2023-04-25 15:19:46 -0600

    Add grouped updates smoke test for go

--- script/run-all.sh
+++ script/run-all.sh
@@ -2,7 +2,7 @@
 
 # This script is useful for regenerating all of the smoke tests running locally.
 
-declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "gradle" "hex" "maven" "npm" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform")
+declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "go-grouped" "gradle" "hex" "maven" "npm" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform")
 for eco in "${arr[@]}"
 do
   dependabot test -f "tests/smoke-$eco.yaml" -o "tests/smoke-$eco.yaml"
