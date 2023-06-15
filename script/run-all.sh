394a3ef6deb1e6bae2c02b36c98b6c093a3b03a3
Bryan Dragon <25506+bdragon@users.noreply.github.com>
2023-06-15 10:02:33 -0600

    Add grouped updates smoke test for yarn berry workspaces

--- script/run-all.sh
+++ script/run-all.sh
@@ -2,7 +2,7 @@
 
 # This script is useful for regenerating all of the smoke tests running locally.
 
-declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "go-group-rules" "gradle" "hex" "maven" "npm" "npm-group-rules" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform" "yarn" "yarn-berry" "yarn-berry-group-rules" "yarn-group-rules")
+declare -a arr=("actions" "bundler" "cargo" "composer" "docker" "elm" "go" "go-group-rules" "gradle" "hex" "maven" "npm" "npm-group-rules" "nuget" "pip" "pip-compile" "pipenv" "poetry" "pub" "submodules" "terraform" "yarn" "yarn-berry" "yarn-berry-group-rules" "yarn-berry-workspaces" "yarn-berry-workspaces-group-rules" "yarn-group-rules")
 for eco in "${arr[@]}"
 do
   dependabot test -f "tests/smoke-$eco.yaml" -o "tests/smoke-$eco.yaml"
