bfd614fc27ebc18abb8f2e80cf453b0ea74c08ca
David Rodriguez <deivid.rodriguez@riseup.net>
2023-09-22 16:09:33 +0200

    Allow regenerating tests keeping cache

--- script/run-all.sh
+++ script/run-all.sh
@@ -4,5 +4,13 @@
 
 for f in tests/*.yaml
 do
-  dependabot test -f "$f" -o "$f"
+  if [ "$1" = "--with-cache" ]
+  then
+    suite=$(echo "$f" | sed -e "s/^tests\/smoke-//" -e "s/\.yaml$//")
+    rm -rf cache
+    source script/download-cache.sh $suite
+    dependabot test -f "$f" -o "$f" --cache cache
+  else
+    dependabot test -f "$f" -o "$f"
+  fi
 done
