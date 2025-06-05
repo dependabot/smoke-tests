488b26b3f5daefc7f39a16db062fc0993454c9e3
Ryan Brandenburg (He/Him) <rybrande@microsoft.com>
2025-06-05 14:03:59 -0700

    Update settings.gradle.kts

--- gradle-lockfile/settings.gradle.kts
+++ gradle-lockfile/settings.gradle.kts
@@ -5,10 +5,5 @@
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.14/userguide/multi_project_builds.html in the Gradle documentation.
  */
 
-plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-}
-
 rootProject.name = "demo"
 include("app")
