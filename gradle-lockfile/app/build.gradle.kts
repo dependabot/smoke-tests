11b12b0d55179610f466dba2c3c1fdc45391e058
Ryan Brandenburg (He/Him) <rybrande@microsoft.com>
2025-06-10 10:22:06 -0700

    Don't use custom task

--- gradle-lockfile/app/build.gradle.kts
+++ gradle-lockfile/app/build.gradle.kts
@@ -28,16 +28,3 @@ application {
     // Define the main class for the application.
     mainClass = "org.example.App"
 }
-
-tasks.register("resolveAndLockAll") {
-    notCompatibleWithConfigurationCache("Filters configurations at execution time")
-    doFirst {
-        require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
-    }
-    doLast {
-        configurations.filter {
-            // Add any custom filtering on the configurations to be resolved
-            it.isCanBeResolved
-        }.forEach { it.resolve() }
-    }
-}
